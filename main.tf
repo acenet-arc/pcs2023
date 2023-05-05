d8c0ccb5dc3b1e7fbd6bbbc421c380673d7ddf25
Chris Geroux <chris.m.geroux@gmail.com>
2023-05-05 16:32:07 -0300

    Baseline PCS 2023 MC Cluster Config

--- main.tf
+++ main.tf
@@ -12,14 +12,15 @@ module "openstack" {
   config_git_url = "https://github.com/ComputeCanada/puppet-magic_castle.git"
   config_version = "12.4.0"
 
-  cluster_name = "phoenix"
-  domain       = "calculquebec.cloud"
+  cluster_name = "pcs2023"
+  domain       = "ace-net.training"
   image        = "Rocky-8"
 
   instances = {
     mgmt   = { type = "p4-6gb", tags = ["puppet", "mgmt", "nfs"], count = 1 }
-    login  = { type = "p2-3gb", tags = ["login", "public", "proxy"], count = 1 }
-    node   = { type = "p2-3gb", tags = ["node"], count = 1 }
+    login  = { type = "p8-12gb", tags = ["login", "public", "proxy"], count = 1 }
+    node   = { type = "p4-6gb", tags = ["node"], count = 1 }
+    gpunode   = { type = "g1-8gb-c4-22gb", tags = ["node"], count = 1 }
   }
 
   # var.pool is managed by Slurm through Terraform REST API.
@@ -36,9 +37,10 @@ module "openstack" {
     }
   }
 
-  public_keys = [file("~/.ssh/id_rsa.pub")]
+  public_keys = ["ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQClvBnQRC2Tesx+357C+RjpN1MxPgbwLrLxSidjl3tInYUVGfwBuNOZ5A+EJBImoCDLZDruioKu2hCM+aR7BlqQQG63wIzIIijTyJsJypmPIH2YpdMPmxY8ntX5ju/mq/F1IBs9hFYWQ5FNwblGl6mxI5HjYcHtNGmfFF0uxmYaTJku+wY0N/GMiTru5XEFVwXh11bg/INaH4GQjbjIn5nlaHBqc2zISqdJ/gc0hPJRnUgYrP0rtlXF3uhQh8Xepu8sM+pcOhqR76ZaJ81SOmDI/zvaejdDjdSEox1TSWNgXSSB6EIdD1lYEEXSWs/d4cIUE7k1X+/j5oVdtBgrBlvB5RNDkBF5m66hh+IgTgFrdX3P1XgSuqZqHy5Zh1Th9di6MqNUt+Y0kd/4Wm54yG1HPbGNKH2mpncWxWa4C2ORkk1TMftONrLvXWbvJVmRLV16koYHE0tezITsrHcivfIx++wOU5eBrcA5mJJknU38vaC135zJ/e7qjTJ2rj9Ja7euqPa34oRzMjMuhJTy3mB9zDuen+gr9XumW/OnUdpqnwg/IlwmuuugWV2KQ7uDFjuBB+ho3ycMCv5zZhmAX7s9Sg3NDoNze88qZG0cDzVWvOBdDe+U+0TTSB+a5TdhzYlX4gEx01Z/UH84SfJGrpxpcEoVxKQB9zsGuNy2ina/Hw== ubuntu@castle-manager"]
+  generate_ssh_key = true
 
-  nb_users = 10
+  nb_users = 100
   # Shared password, randomly chosen if blank
   guest_passwd = ""
 }
@@ -51,16 +53,16 @@ output "public_ip" {
   value = module.openstack.public_ip
 }
 
-## Uncomment to register your domain name with CloudFlare
-# module "dns" {
-#   source           = "./dns/cloudflare"
-#   email            = "you@example.com"
-#   name             = module.openstack.cluster_name
-#   domain           = module.openstack.domain
-#   public_instances = module.openstack.public_instances
-#   ssh_private_key  = module.openstack.ssh_private_key
-#   sudoer_username  = module.openstack.accounts.sudoer.username
-# }
+# Uncomment to register your domain name with CloudFlare
+module "dns" {
+  source           = "./dns/cloudflare"
+  email            = "chris.geroux@ace-net.ca"
+  name             = module.openstack.cluster_name
+  domain           = module.openstack.domain
+  public_instances = module.openstack.public_instances
+  ssh_private_key  = module.openstack.ssh_private_key
+  sudoer_username  = module.openstack.accounts.sudoer.username
+}
 
 ## Uncomment to register your domain name with Google Cloud
 # module "dns" {
@@ -75,6 +77,6 @@ output "public_ip" {
 #   sudoer_username  = module.openstack.accounts.sudoer.username
 # }
 
-# output "hostnames" {
-#   value = module.dns.hostnames
-# }
+output "hostnames" {
+  value = module.dns.hostnames
+}
